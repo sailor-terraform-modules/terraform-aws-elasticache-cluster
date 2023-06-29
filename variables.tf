07547cd588e33a353f615ea82613f085037001db
Vijay-bootlabs <vijay@bootlabstech.com>
2023-06-29 14:11:05 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "cluster_id" {
+  type        = string
+  description = "Group identifier."
+}
+variable "engine" {
+  type        = string
+  description = "Name of the cache engine to be used for this cache cluster."
+}
+variable "node_type" {
+  type        = string
+  description = "The instance class used."
+}
+variable "num_cache_nodes" {
+  type        = number
+  description = "The initial number of cache nodes that the cache cluster will have."
+}
+variable "parameter_group_name" {
+  type        = string
+  description = "The name of the parameter group to associate with this cache cluster."
+}
+variable "port" {
+  type        = number
+  description = "The port number on which each of the cache nodes will accept connections."
+}
+variable "region" {
+  type        = string
+  description = "region"
+}
